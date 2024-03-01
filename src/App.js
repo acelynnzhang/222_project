7ab8fb9eaee5934d6078161eca4b3132d9a01e87
Dev Patel <devpatel@wirelessprv-10-192-149-106.near.illinois.edu>
2024-03-01 15:57:44 -0600

    initial main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './index.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <h1 className="title">Temporary Name</h1>
+      <input
+        className="inputBox"
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter a class name!"
+      />
     </div>
   );
 }
+
 
 export default App;
