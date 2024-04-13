cc0f003d10108bfc4c4a0c500f4236a678c4d138
acelynnzhang <azyx0987e@gmail.com>
2024-04-13 18:52:19 -0500

    otherroute

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,24 @@
 from flask import Flask
-import testing
+import functions
+
 
 app = Flask(__name__)
 
+profinfo = {}
+
 @app.route('/courselookup/<course>/<number>')
 def lookup(course, number):
-    testing.func(f'{course} {number}')
+    global profinfo
+    profinfo = functions.func(f'{course} {number}')
+    return profinfo
+
+
+@app.route('/prof/<proffirst>/<proflast>')
+def sum(proffirst,proflast):
+    global profinfo
+    if len(profinfo) < 1:
+        raise Exception("no class given")
+    prof = f'{proffirst}, {proflast}'
+    if prof not in profinfo:
+        raise Exception("prof not in info")
+    return functions.fetchprof(profinfo[prof][0].ratemyprofid), 200
