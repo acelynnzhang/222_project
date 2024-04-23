e2301baf38337159887f23775fb160f0a2d33a0c
Tianyi Zhong <zhongtianyi26@LAPTOP-2U5GPUET>
2024-04-22 21:13:04 -0500

    change summarize

--- backend/summarize.py
+++ backend/summarize.py
@@ -2,16 +2,34 @@ import ollama
 
 #need id number from here, willgive previewdata
 
-def summarize(strt): 
-    response = ollama.chat(model='llama3', messages=[
+
+def find_comments(dictionary):
+    comments = []
+
+    def find_in_dict(dictionary):
+        for key, value in dictionary.items():
+            if isinstance(value, list):
+                for l in value:
+                    find_in_dict(l)
+            if key == "comment":
+                comments.append(value)
+            if isinstance(value, dict):
+                find_in_dict(value)
+
+    find_in_dict(dictionary)
+    return comments
+
+def summarize(json_str): 
+    comments = find_comments(json_str)
+
+    '''response = ollama.chat(model='llama2', messages=[
     {
         'role': 'user',
         'content': 'extract all the sentences comes after "comment" in the following message:\n' 
-        + strt 
+        + str 
         + '\n then try to summarize each comments into several keywords followed by a summarized sentence of comment'
     }
     ])
-    print(response['message']['content'])
-    return response['message']['content']
+    return response['message']['content']'''
     
-#summarize("hi there mfs i die now")
+
